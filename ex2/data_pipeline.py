8a60a55fb577e6939bc03798ea3a5b5d4b805329
Matheo Gerard <mgerard@fu-r4-p7.s19.be>
2026-06-14 16:53:25 +0200

    add ex2

---
+++ ex2/data_pipeline.py
@@ -0,0 +1,214 @@
+from abc import ABC, abstractmethod
+import typing
+
+
+class DataProcessor(ABC):
+    def __init__(self) -> None:
+        self.rank: int = 0
+        self.data: list[str] = []
+        self.total_processed = 0
+
+    @abstractmethod
+    def validate(self, data: typing.Any) -> bool:
+        pass
+
+    @abstractmethod
+    def ingest(self, data: typing.Any) -> None:
+        pass
+
+    def output(self) -> tuple[int, str]:
+        tuple_to_return: tuple[int, str] = (self.rank, self.data[0])
+        self.data.remove(tuple_to_return[1])
+        self.rank += 1
+        return tuple_to_return
+
+
+class NumericProcessor(DataProcessor):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def validate(self, data: typing.Any) -> bool:
+        if isinstance(data, (float, int)):
+            return True
+        elif isinstance(data, list) and all(
+            isinstance(x, (int, float)) for x in data
+        ):
+            return True
+        else:
+            return False
+
+    def ingest(self, data: int | float | list[int | float]) -> None:
+        if isinstance(data, (int, float)):
+            self.data.append(str(data))
+            self.total_processed += 1
+        elif isinstance(data, list):
+            for x in data:
+                self.data.append(str(x))
+                self.total_processed += 1
+        else:
+            raise TypeError("Got exception: Improper numeric data")
+
+
+class TextProcessor(DataProcessor):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def validate(self, data: typing.Any) -> bool:
+        if isinstance(data, str):
+            return True
+        elif isinstance(data, list) and all(isinstance(x, str) for x in data):
+            return True
+        else:
+            return False
+
+    def ingest(self, data: str | list[str]) -> None:
+        if isinstance(data, (str)):
+            self.data.append(data)
+            self.total_processed += 1
+        elif isinstance(data, list):
+            for x in data:
+                self.data.append(x)
+                self.total_processed += 1
+        else:
+            raise TypeError("Got exception: Improper numeric data")
+
+
+class LogProcessor(DataProcessor):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def validate(self, data: typing.Any) -> bool:
+        if isinstance(data, dict) and all(
+            isinstance(k, str) and isinstance(v, str) for k, v in data.items()
+        ):
+            return True
+        elif isinstance(data, list) and all(
+            isinstance(x, dict)
+            and all(
+                isinstance(k, str) and isinstance(v, str) for k, v in x.items()
+            )
+            for x in data
+        ):
+            return True
+        else:
+            return False
+
+    def ingest(self, data: dict[str, str] | list[dict[str, str]]) -> None:
+        if isinstance(data, dict):
+            self.data.append(f"{data['log_level']}: {data['log_message']}")
+            self.total_processed += 1
+        elif isinstance(data, list):
+            for x in data:
+                self.data.append(f"{x['log_level']}: {x['log_message']}")
+                self.total_processed += 1
+        else:
+            raise TypeError("Got exception: Improper numeric data")
+
+
+class DataStream:
+    def __init__(self) -> None:
+        self.num_proc: NumericProcessor | None = None
+        self.text_proc: TextProcessor | None = None
+        self.log_proc: LogProcessor | None = None
+
+    def register_processor(self, proc: DataProcessor) -> None:
+        if isinstance(proc, NumericProcessor):
+            self.num_proc = proc
+        elif isinstance(proc, TextProcessor):
+            self.text_proc = proc
+        elif isinstance(proc, LogProcessor):
+            self.log_proc = proc
+        else:
+            raise Exception("This type of proc is not supported")
+
+    def process_stream(self, stream: list[typing.Any]) -> None:
+        for x in stream:
+            if self.num_proc and self.num_proc.validate(x):
+                self.num_proc.ingest(x)
+            elif self.text_proc and self.text_proc.validate(x):
+                self.text_proc.ingest(x)
+            elif self.log_proc and self.log_proc.validate(x):
+                self.log_proc.ingest(x)
+            else:
+                print(
+                    f"DataStream error - Can't process element in stream: {x}"
+                )
+
+    def print_processors_stats(self) -> None:
+        if not self.num_proc and not self.text_proc and not self.log_proc:
+            print("No processor found, no data")
+        if self.num_proc:
+            print(
+                f"Numeric Processor: total {self.num_proc.total_processed} "
+                f"items processed, remaining {len(self.num_proc.data)} on "
+                "processor"
+            )
+        if self.text_proc:
+            print(
+                f"Text Processor: total {self.text_proc.total_processed} "
+                f"items processed, remaining {len(self.text_proc.data)} on "
+                "processor"
+            )
+        if self.log_proc:
+            print(
+                f"Log Processor: total {self.log_proc.total_processed} "
+                f"items processed, remaining {len(self.log_proc.data)} on "
+                "processor"
+            )
+
+
+if __name__ == "__main__":
+    print("=== Code Nexus - Data Stream ===")
+    print()
+    print("Initialize Data Stream...")
+    data_stream: DataStream = DataStream()
+    print("== DataStream statistics ==")
+    data_stream.print_processors_stats()
+    print()
+    print("Registering Numeric Processor")
+    num_proc: NumericProcessor = NumericProcessor()
+    data_stream.register_processor(num_proc)
+    print()
+    data: list[typing.Any] = [
+        "Hello world",
+        [3.14, -1, 2.71],
+        [
+            {
+                "log_level": "WARNING",
+                "log_message": "Telnet access! Use ssh instead",
+            },
+            {"log_level": "INFO", "log_message": "User wil is connected"},
+        ],
+        42,
+        ["Hi", "five"],
+    ]
+    print(f"Send first batch of data on stream: {data}")
+    data_stream.process_stream(data)
+    print("== DataStream statistics ==")
+    data_stream.print_processors_stats()
+    print()
+    print("Registering other data processors")
+    text_proc: TextProcessor = TextProcessor()
+    log_proc: LogProcessor = LogProcessor()
+    data_stream.register_processor(text_proc)
+    data_stream.register_processor(log_proc)
+    print("Send the same batch again")
+    data_stream.process_stream(data)
+    print("== DataStream statistics ==")
+    data_stream.print_processors_stats()
+    print()
+    consume_num: int = 3
+    consume_text: int = 2
+    consume_log: int = 1
+    print(
+        "Consume some elements from the data processors: Numeric "
+        f"{consume_num}, Text {consume_text}, Log {consume_log}"
+    )
+    for _ in range(0, consume_num):
+        num_proc.output()
+    for _ in range(0, consume_text):
+        text_proc.output()
+    for _ in range(0, consume_log):
+        log_proc.output()
+    print("== DataStream statistics ==")
+    data_stream.print_processors_stats()
